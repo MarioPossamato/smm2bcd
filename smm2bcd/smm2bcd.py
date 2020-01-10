680b0ccf342a1ccb90b2254e0e5251e086f64ed0
Mario Possamato <51346942+MarioPossamato@users.noreply.github.com>
2020-01-10 16:36:22 -0500

    Add files via upload

--- smm2bcd/smm2bcd.py
+++ smm2bcd/smm2bcd.py
@@ -10,7 +10,7 @@ def open_enc_course():
     course_path = filedialog.askopenfilename(initialdir = home + "/Desktop",title = "Select An Encrypted Super Mario Maker 2 Course Data File",filetypes = (("Binary Course Data Files","*.bcd"),("All Files","*.*")))
     if course_path:
         head, tail = os.path.split(course_path)
-        dec_course_path = head + '/dec_' + tail
+        dec_course_path = head + '\dec_' + tail
         with open('path/decryptor_path.txt','r') as decryptor_path:
             dec_path = decryptor_path.read()
             os.system(dec_path + ' ' + course_path + ' ' + dec_course_path)
@@ -24,7 +24,7 @@ def import_enc_course_path():
     with open('path/enc_course_path.txt','r') as course_path:
         enc_course_path = course_path.read()
         head, tail = os.path.split(enc_course_path)
-        dec_course_path = head + '/dec_' + tail
+        dec_course_path = head + '\dec_' + tail
         with open('path/decryptor_path.txt','r') as decryptor_path:
             dec_path = decryptor_path.read()
             os.system(dec_path + ' ' + enc_course_path + ' ' + dec_course_path)
@@ -634,8 +634,248 @@ def print_entities():
     print('ID ' + str(id) + ', ' + hex(id) + ': ! Block')
 
 
-def edit_entity(entity_number, new_id):
+def edit_entity(entity_name, entity_number):
     global dec_course_path
+    if entity_name == 'Goomba' or entity_name == 'Galoomba':
+        new_id = 0x00
+    if entity_name == 'Koopa Troopa':
+        new_id = 0x01
+    if entity_name == 'Piranha Plant' or entity_name == 'Jumping Piranha Plant':
+        new_id = 0x02
+    if entity_name == 'Hammer Bro':
+        new_id = 0x03
+    if entity_name == 'Block':
+        new_id = 0x04
+    if entity_name == '? Block':
+        new_id = 0x05
+    if entity_name == 'Hard Block':
+        new_id = 0x06
+    if entity_name == 'Ground':
+        new_id = 0x07
+    if entity_name == 'Coin':
+        new_id = 0x08
+    if entity_name == 'Pipe':
+        new_id = 0x09
+    if entity_name == 'Trampoline':
+        new_id = 0x0A
+    if entity_name == 'Lift' or entity_name == 'Cloud Lift':
+        new_id = 0x0B
+    if entity_name == 'Thwomp':
+        new_id = 0x0C
+    if entity_name == 'Bill Blaster':
+        new_id = 0x0D
+    if entity_name == 'Mushroom Platform':
+        new_id = 0x0E
+    if entity_name == 'Bob-Omb':
+        new_id = 0x0F
+    if entity_name == 'Semisolid Platform':
+        new_id = 0x10
+    if entity_name == 'Bridge':
+        new_id = 0x11
+    if entity_name == 'P-Switch':
+        new_id = 0x12
+    if entity_name == 'Pow Block':
+        new_id = 0x13
+    if entity_name == 'Super Mushroom':
+        new_id = 0x14
+    if entity_name == 'Donut Block':
+        new_id = 0x15
+    if entity_name == 'Cloud Block':
+        new_id = 0x16
+    if entity_name == 'Note Block':
+        new_id = 0x17
+    if entity_name == 'Firebar':
+        new_id = 0x18
+    if entity_name == 'Spiny':
+        new_id = 0x19
+    if entity_name == 'Ground Goal':
+        new_id = 0x1A
+    if entity_name == 'Goal Pole':
+        new_id = 0x1B
+    if entity_name == 'Buzzy Beatle':
+        new_id = 0x1C
+    if entity_name == 'Hidden Block':
+        new_id = 0x1D
+    if entity_name == 'Lakitu':
+        new_id = 0x1E
+    if entity_name == 'Cloud':
+        new_id = 0x1F
+    if entity_name == 'Banzai Bill':
+        new_id = 0x20
+    if entity_name == '1-Up Mushroom':
+        new_id = 0x21
+    if entity_name == 'Fire Flower':
+        new_id = 0x22
+    if entity_name == 'Super Star':
+        new_id = 0x23
+    if entity_name == 'Lava lift':
+        new_id = 0x24
+    if entity_name == 'Ground Start':
+        new_id = 0x25
+    if entity_name == 'Start Arrow':
+        new_id = 0x26
+    if entity_name == 'Kameck':
+        new_id = 0x27
+    if entity_name == 'Spike Top':
+        new_id = 0x28
+    if entity_name == 'Boo':
+        new_id = 0x29
+    if entity_name == 'Koopa Clown Car':
+        new_id = 0x2A
+    if entity_name == 'Spike Trap':
+        new_id = 0x2B
+    if entity_name == '3rd Tier Powerup':
+        new_id = 0x2C
+    if entity_name == 'Shoe Goomba' or entity_name == 'Yoshi Egg':
+        new_id = 0x2D
+    if entity_name == 'Dry Bones':
+        new_id = 0x2E
+    if entity_name == 'Cannon':
+        new_id = 0x2F
+    if entity_name == 'Blooper':
+        new_id = 0x30
+    if entity_name == 'Castle Bridge':
+        new_id = 0x31
+    if entity_name == 'Hop-Chops':
+        new_id = 0x32
+    if entity_name == 'Skipsqueak':
+        new_id = 0x33
+    if entity_name == 'Wiggler':
+        new_id = 0x34
+    if entity_name == 'Conveyer Belt':
+        new_id = 0x35
+    if entity_name == 'Burner':
+        new_id = 0x36
+    if entity_name == 'Warp Door':
+        new_id = 0x37
+    if entity_name == 'Cheep Cheep':
+        new_id = 0x38
+    if entity_name == 'Muncher':
+        new_id = 0x39
+    if entity_name == 'Rocky Wrench':
+        new_id = 0x3A
+    if entity_name == 'Rail':
+        new_id = 0x3B
+    if entity_name == 'Lava Bubble':
+        new_id = 0x3C
+    if entity_name == 'Chain Chomp':
+        new_id = 0x3D
+    if entity_name == 'Bowser' or entity_name == 'Meowser':
+        new_id = 0x3E
+    if entity_name == 'Ice Block':
+        new_id = 0x3F
+    if entity_name == 'Vine':
+        new_id = 0x40
+    if entity_name == 'Stingby':
+        new_id = 0x41
+    if entity_name == 'Arrow Sign':
+        new_id = 0x42
+    if entity_name == 'One-Way Wall':
+        new_id = 0x43
+    if entity_name == 'Grinder':
+        new_id = 0x44
+    if entity_name == 'Player':
+        new_id = 0x45
+    if entity_name == '10-Coin':
+        new_id = 0x46
+    if entity_name == 'Semisolid Platform (3D World)':
+        new_id = 0x47
+    if entity_name == 'Koopa Troopa Car':
+        new_id = 0x48
+    if entity_name == 'Toad':
+        new_id = 0x49
+    if entity_name == 'None':
+        new_id = 0x4A
+    if entity_name == 'Stone':
+        new_id = 0x4B
+    if entity_name == 'Twister':
+        new_id = 0x4C
+    if entity_name == 'Boom Boom':
+        new_id = 0x4D
+    if entity_name == 'None':
+        new_id = 0x4E
+    if entity_name == 'None':
+        new_id = 0x4F
+    if entity_name == 'None':
+        new_id = 0x50
+    if entity_name == 'None':
+        new_id = 0x51
+    if entity_name == 'Bumper':
+        new_id = 0x52
+    if entity_name == 'Spike Pillar':
+        new_id = 0x53
+    if entity_name == 'Snake Block':
+        new_id = 0x54
+    if entity_name == 'Track Block':
+        new_id = 0x55
+    if entity_name == 'Charvaargh':
+        new_id = 0x56
+    if entity_name == 'Gentle Slope':
+        new_id = 0x57
+    if entity_name == 'Steep Slope':
+        new_id = 0x58
+    if entity_name == 'Custom Scroll Waypoint':
+        new_id = 0x59
+    if entity_name == 'Checkpoint Flag':
+        new_id = 0x5A
+    if entity_name == 'Seesaw':
+        new_id = 0x5B
+    if entity_name == 'Pink Coin':
+        new_id = 0x5C
+    if entity_name == 'Clear Pipe':
+        new_id = 0x5D
+    if entity_name == 'Sloped Conveyer Belt':
+        new_id = 0x5E
+    if entity_name == 'Key':
+        new_id = 0x5F
+    if entity_name == 'Ant Trooper':
+        new_id = 0x60
+    if entity_name == 'Warp Box':
+        new_id = 0x61
+    if entity_name == 'Bowser Jr.':
+        new_id = 0x62
+    if entity_name == 'On/Off Switch':
+        new_id = 0x63
+    if entity_name == 'Dotted-Line Block':
+        new_id = 0x64
+    if entity_name == 'Lava Editor':
+        new_id = 0x65
+    if entity_name == 'Monty Mole':
+        new_id = 0x66
+    if entity_name == 'Fish Bones':
+        new_id = 0x67
+    if entity_name == 'Angry Sun':
+        new_id = 0x68
+    if entity_name == 'Swinging Claw':
+        new_id = 0x69
+    if entity_name == 'Tree':
+        new_id = 0x6A
+    if entity_name == 'Piranha Creeper':
+        new_id = 0x6B
+    if entity_name == 'Blinking Block':
+        new_id = 0x6C
+    if entity_name == 'Sound Effect Icon':
+        new_id = 0x6D
+    if entity_name == 'Spike Block':
+        new_id = 0x6E
+    if entity_name == 'None':
+        new_id = 0x6F
+    if entity_name == 'Crate':
+        new_id = 0x70
+    if entity_name == 'Mushroom Trampoline':
+        new_id = 0x71
+    if entity_name == 'Porcupuffer':
+        new_id = 0x72
+    if entity_name == 'Goal Toadette':
+        new_id = 0x73
+    if entity_name == 'Super Hammer':
+        new_id = 0x74
+    if entity_name == 'Bully':
+        new_id = 0x75
+    if entity_name == 'Icicle':
+        new_id = 0x76
+    if entity_name == '! Block':
+        new_id = 0x77
     with open(dec_course_path, 'rb') as course:
 	    data = course.read()
 	    data = bytearray(data)
@@ -646,10 +886,25 @@ def edit_entity(entity_number, new_id):
 
 
 def encrypt_course():
-    print('This feature has not been implemented yet!')
+    print("This feature has not been implemented yet, please don't try to use it!")
     pass
     global dec_course_path
     with open('path/decryptor_path.txt','r') as decryptor_path:
             dec_path = decryptor_path.read()
             os.system(dec_path + ' ' + dec_course_path + ' ' + str(dec_course_path).replace('dec_',''))
             print('Successfully Encrypted!')
+
+
+def edit_key_status(entity_number, key_status):
+    global dec_course_path
+    if key_status == False:
+        key = 0x00
+    if key_status == True:
+        key = 0x12
+    with open(dec_course_path, 'rb') as course:
+        data = course.read()
+        data = bytearray(data)
+        data[577 + 32 * entity_number] = int(key)
+        with open(dec_course_path, 'wb') as course:
+            course.write(data)
+            print('Success!')
