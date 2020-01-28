2ccc421f4b28b0e68abf385f5a20600887a1aa15
Mario Possamato <51346942+MarioPossamato@users.noreply.github.com>
2020-01-27 23:39:47 -0500

    Add files via upload

--- smm2bcd/smm2bcd.py
+++ smm2bcd/smm2bcd.py
@@ -1185,6 +1185,7 @@ def add_new_basic_entity():
                 entity_name = 'Icicle'
             if entity_id == bytes([0x76, 0x00, 0xFF]) or entity_id == bytes([0x76, 0x12, 0xFF]):
                 entity_name = '! Block'
+            print(str(i) + ': ' + str(entity_name) + ' @' + str(hex(608 + 32 * i)))
             if entity_name == f'Unknown({entity_id})':
                 with open(dec_course_path,'rb') as course:
                     data = course.read()
